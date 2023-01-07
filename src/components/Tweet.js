79c39822c1d1b48f149150a049fdf6c79f5569f4
Curbelman <111905398+Curbelman@users.noreply.github.com>
2023-01-07 20:09:53 +0100

    adds actions, completes iteration 8

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -2,6 +2,7 @@ import ProfileImage from "./ProfileImage";
 import User from "./User";
 import Timestamp from "./Timestamp";
 import Message from "./Message";
+import Actions from "./Actions";
 
 function Tweet(props) {
   return (
@@ -12,19 +13,10 @@ function Tweet(props) {
         <User userData={props.tweet.user} />
         <Timestamp time={props.tweet.timestamp} />
         </div>
-
         <Message message={props.tweet.message} />
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
+        <Actions />
         </div>
-      </div>
-
-      <i className="fas fa-ellipsis-h"></i>
+          <i className="fas fa-ellipsis-h"></i>
     </div>
   );
 }
