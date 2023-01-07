9b75bd546bc1b7e5722d9023e47782c4d4312db6
Curbelman <111905398+Curbelman@users.noreply.github.com>
2023-01-07 20:16:57 +0100

    adds 3 full tweet array, lab done

--- src/App.js
+++ src/App.js
@@ -37,7 +37,9 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet tweet={ tweetsArray[0] }/>
+    {tweetsArray.map((tweets, i) => {
+      return <Tweet tweet={tweets} key={i} />
+    })}
     </div>
   );
 }
